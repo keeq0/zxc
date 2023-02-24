0428f133cd864bcbb9c6cfbcdb7725d16b208d23
keeq0 <myakotnykh1689@gmail.com>
2023-02-24 21:03:46 +0300

    second commit

---
+++ praktika3-zadanie1/classification.h
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class eq2 {
+private:
+	double a, b, c; // Коэффициенты уравнения
+	double D; // Дискриминант
+public:
+	eq2(double a1, double b1, double c1) {
+		a = a1;
+		b = b1;
+		c = c1;
+		D = b * b - 4 * a * c;
+	}
+
+	void set(double a1, double b1, double c1) {
+		a = a1;
+		b = b1;
+		c = c1;
+		D = b * b - 4 * a * c;
+	}
+
+	double find_X() {
+		if (D < 0) {
+			cout << "Корней нет" << endl;
+		}
+		else {
+			if (D == 0) {
+				cout << "Корень один: ";
+				double x = (b * sqrt(b)) / (2 * a);
+				cout << x << endl;
+				return x;
+			}
+			else {
+				cout << "Корней 2, наибольший: ";
+				double x1 = (-b - sqrt(D)) / (2 * a);
+				double x2 = (-b + sqrt(D)) / (2 * a);
+				cout << max(x1, x2);
+				return max(x1, x2);
+			}
+		}
+	}
+
+	double find_Y(double x1) {
+		return a * x1 * x1 + b * x1 + c;
+	}
+
+ };
---
+++ praktika3-zadanie1/praktika3-zadanie1.cpp
@@ -0,0 +1,20 @@
+﻿#include "classification.h";
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите коэффициенты уравнения a,b,c через пробел соответственно: ";
+
+	double a, b, c;
+	cin >> a >> b >> c;
+
+	eq2 A(a, b, c);
+	A.find_X();
+	cout << A.find_Y(6) << endl;
+	return 0;
+}
