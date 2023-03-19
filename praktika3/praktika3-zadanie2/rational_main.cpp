d5efd198af438d2fd0e1e5a57c45306adac2ba66
keeq0 <myakotnykh1689@gmail.com>
2023-03-19 09:20:57 +0300

    pr3-z3

--- praktika3/praktika3-zadanie2/rational_main.cpp
+++ praktika3/praktika3-zadanie2/rational_main.cpp
@@ -12,7 +12,7 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cout << "Введите через пробел числитель и знаменатель для дроби №" << i+1 << ": "; cin >> a >> b;
-		mass[i].set(a, b);
+		mass[i].set(a, b); 
 	}
 
 	cout << endl;
