d2c4eef4bac86f35217ae07272f9b154e8514177
keeq0 <myakotnykh1689@gmail.com>
2023-04-08 10:45:04 +0300

    3,4 kursach

---
+++ krusach/krusach.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <windows.h>
+
+class Product // Объявление классa Product
+{
+private: // Объявление полей
+	std::string name;
+	int quantity;
+	double price;
+public: // Объявление конструктора Product и методов
+	Product(std::string n, int q, double p)
+	{
+		name = n;
+		quantity = q;
+		price = p;
+	}
+
+	std::string getName() const
+	{
+		return name;
+	}
+
+	int getQuantity() const
+	{
+		return quantity;
+	}
+
+	int getPrice() const
+	{
+		return price;
+	}
+
+	void setQuantity(int q)
+	{
+		quantity = q;
+	}
+
+	void setPrice(int p)
+	{
+		price = p;
+	}
+
+	double getTotalValue() const
+	{
+		return price * quantity;
+	}
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+}
